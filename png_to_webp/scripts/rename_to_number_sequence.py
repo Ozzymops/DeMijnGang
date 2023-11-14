047e0f5075fb538b917080619729d9b5919cc1c5
Ozzymops <39087842+Ozzymops@users.noreply.github.com>
2023-11-14 10:22:06 +0100

    Update rename_to_number_sequence.py
    added option to start from specific number

--- png_to_webp/scripts/rename_to_number_sequence.py
+++ png_to_webp/scripts/rename_to_number_sequence.py
@@ -7,6 +7,11 @@ def dir_check(path):
         return False
     return True
 
+# ask to start from 0 or later number
+def give_input():
+    print("> Start from beginning or from specific number? [1/#]")
+    return input("> ")
+
 def main():
     increment = 1
     path_output = os.path.abspath("../../output/")
@@ -15,6 +20,20 @@ def main():
     if (dir_check(path_output) == False):
         print("> Output directory is empty. Cancelling...")
         return
+        
+    # ask for input
+    input_given = False
+    while input_given == False:
+        input = give_input()
+        
+        try:
+            int(input)
+        except ValueError:
+            print("Input is not valid. Try again.")
+               
+        input_given = True
+        if int(input) > 1:
+            increment = int(input)
         
     print("> Renaming images...")
     for image in os.listdir(path_output):
