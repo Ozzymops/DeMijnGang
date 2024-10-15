6835e6ac524f038188514e571ee5319c815bcd3b
Ozzymops <justinmuris@gmail.com>
2024-10-15 11:04:39 +0200

    Plugin code cleanup

---
+++ Projects/WordpressPlugins/events-manager-mollie/em-mollie.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: Events Manager - Mollie
+ * Description: Adds support for Mollie to Events Manager (Pro).
+ * Version: 1.0
+ * Author: Justin Muris / Ozzymops
+ * Requires Plugins: events-manager, events-manager-pro
+ */
+
+if (!defined('ABSPATH')) exit;
