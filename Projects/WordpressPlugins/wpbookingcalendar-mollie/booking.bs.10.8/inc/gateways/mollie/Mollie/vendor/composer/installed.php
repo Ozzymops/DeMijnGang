9365d753a49130e0fdad87f4c84aafa600080356
Ozzymops <justinmuris@gmail.com>
2025-01-31 12:29:45 +0100

    Restructure / clean up

--- Projects/WordpressPlugins/wpbookingcalendar-mollie/booking.bs.10.8/inc/gateways/mollie/Mollie/vendor/composer/installed.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace _PhpScoper356dca4c8f10;
-
-return array('root' => array('name' => 'mollie/mollie-api-php', 'pretty_version' => 'v2.75.0', 'version' => '2.75.0.0', 'reference' => 'ac2052855c631b90092768cf6657f6d5b2f5dd9f', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev' => \false), 'versions' => array('composer/ca-bundle' => array('pretty_version' => '1.5.3', 'version' => '1.5.3.0', 'reference' => '3b1fc3f0be055baa7c6258b1467849c3e8204eb2', 'type' => 'library', 'install_path' => __DIR__ . '/./ca-bundle', 'aliases' => array(), 'dev_requirement' => \false), 'mollie/mollie-api-php' => array('pretty_version' => 'v2.75.0', 'version' => '2.75.0.0', 'reference' => 'ac2052855c631b90092768cf6657f6d5b2f5dd9f', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev_requirement' => \false)));
